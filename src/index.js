df1c14599796c5313c50eb074dfeabca00179242
AlenaShkr <elalshkr@gmail.com>
2019-02-20 20:51:24 +0300

    money-exchange:add method that determine the minimum number of coins needed to make change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let countCoins = {};
+    const rating = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+
+    if (currency > 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else if (currency <= 0){
+        return countCoins;
+    } else {
+        for (let key in rating){
+            if (currency >= rating[key]){
+                const count = Math.floor(currency / rating[key]);
+                currency = currency % rating[key];
+                countCoins[key] = count;
+            }
+        }
+        return countCoins;
+    }
 }
